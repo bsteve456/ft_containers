1e6a0fe0696df2ec0dce7055f649cbb6a0b54e8f
bsteve456 <stevealan1@protonmail.com>
2021-02-17 14:23:59 +0100

    6 out of 6

--- List/test_list/true_list.cpp
+++ List/test_list/true_list.cpp
@@ -277,7 +277,7 @@ int main()
 	if (b<c) std::cout << "b is less than c\n";
 	if (c>b) std::cout << "c is greater than b\n";
 	if (a<=b) std::cout << "a is less than or equal to b\n";
-//	if (a>=b) std::cout << "a is greater than or equal to b\n";
+	if (a>=b) std::cout << "a is greater than or equal to b\n";
 
 }
 
