9338e51bcec9070024de49336b4bffa113a80461
bsteve456 <stevealan1@protonmail.com>
2021-03-09 13:54:54 +0100

    add iterator_tag to namespace ft

--- iterator_tag.hpp
+++ iterator_tag.hpp
@@ -1,120 +1,124 @@
 #ifndef ITERATOR_TAG
 # define ITERATOR_TAG
 
-struct input_iterator_tag { };
-struct output_iterator_tag { };
-struct forward_iterator_tag : public input_iterator_tag { };
-struct bidirectional_iterator_tag : public forward_iterator_tag { };
-struct random_access_iterator_tag : public bidirectional_iterator_tag { };
 
-template<bool flag, class IsTrue, class IsFalse>
-struct choose;
-
-template <class IsTrue, class IsFalse>
-struct choose<true, IsTrue, IsFalse>
+namespace ft
 {
-	typedef IsTrue type;
-};
-template <class IsTrue, class IsFalse>
-struct choose<false, IsTrue, IsFalse>
-{
-	typedef IsFalse type;
-};
 
-template <typename T>
-struct is_integral_float
-{
-	static const bool value = false;
+	struct input_iterator_tag { };
+	struct output_iterator_tag { };
+	struct forward_iterator_tag : public input_iterator_tag { };
+	struct bidirectional_iterator_tag : public forward_iterator_tag { };
+	struct random_access_iterator_tag : public bidirectional_iterator_tag { };
+
+	template<bool flag, class IsTrue, class IsFalse>
+		struct choose;
+
+	template <class IsTrue, class IsFalse>
+		struct choose<true, IsTrue, IsFalse>
+		{
+			typedef IsTrue type;
+		};
+	template <class IsTrue, class IsFalse>
+		struct choose<false, IsTrue, IsFalse>
+		{
+			typedef IsFalse type;
+		};
+
+	template <typename T>
+		struct is_integral_float
+		{
+			static const bool value = false;
+		};
+
+	template<>
+		struct is_integral_float<int>
+		{
+			static const bool value = true;
+		};
+
+	template<>
+		struct is_integral_float<char>
+		{
+			static const bool value = true;
+		};
+
+	template<>
+		struct is_integral_float<long>
+		{
+			static const bool value = true;
+		};
+
+	template<>
+		struct is_integral_float<short>
+		{
+			static const bool value = true;
+		};
+
+	template<>
+		struct is_integral_float<double>
+		{
+			static const bool value = true;
+		};
+
+	template<>
+		struct is_integral_float<float>
+		{
+			static const bool value = true;
+		};
+
+	template<>
+		struct is_integral_float<bool>
+		{
+			static const bool value = true;
+		};
+
+
+	template <bool, typename T = void>
+		struct enable_if
+		{};
+
+	template <typename T>
+		struct enable_if<true, T> {
+			typedef T type;
+		};
+
+	template <typename T, typename = void>
+		struct is_pointer
+		{
+			static const bool value = false;
+		};
+
+	template<typename T>
+		struct is_pointer<T *>
+		{
+			static const bool value = true;
+		};
+	template <class U, class T>
+		struct is_same
+		{
+			static const bool value = false;
+		};
+
+	template <class U>
+		struct is_same<U, U>
+		{
+			static const bool value = true;
+		};
+
+	template <bool, class U>
+		struct is_input_iterator
+		{
+			static const bool value = false;
+		};
+
+	template <class U>
+		struct is_input_iterator<false, U>
+		{
+			static const bool value = is_same<typename U::iterator_category,bidirectional_iterator_tag>::value |
+				is_same<typename U::iterator_category,input_iterator_tag>::value |
+				is_same<typename U::iterator_category,forward_iterator_tag>::value |
+				is_same<typename U::iterator_category,random_access_iterator_tag>::value;
+		};
 };
-
-template<>
-struct is_integral_float<int>
-{
-	static const bool value = true;
-};
-
-template<>
-struct is_integral_float<char>
-{
-	static const bool value = true;
-};
-
-template<>
-struct is_integral_float<long>
-{
-	static const bool value = true;
-};
-
-template<>
-struct is_integral_float<short>
-{
-	static const bool value = true;
-};
-
-template<>
-struct is_integral_float<double>
-{
-	static const bool value = true;
-};
-
-template<>
-struct is_integral_float<float>
-{
-	static const bool value = true;
-};
-
-template<>
-struct is_integral_float<bool>
-{
-	static const bool value = true;
-};
-
-
-template <bool, typename T = void>
-struct enable_if
-{};
-
-template <typename T>
-struct enable_if<true, T> {
-	typedef T type;
-};
-
-template <typename T, typename = void>
-struct is_pointer
-{
-	static const bool value = false;
-};
-
-template<typename T>
-struct is_pointer<T *>
-{
-	static const bool value = true;
-};
-template <class U, class T>
-struct is_same
-{
-	static const bool value = false;
-};
-
-template <class U>
-struct is_same<U, U>
-{
-	static const bool value = true;
-};
-
-template <bool, class U>
-struct is_input_iterator
-{
-	static const bool value = false;
-};
-
-template <class U>
-struct is_input_iterator<false, U>
-{
-	static const bool value = is_same<typename U::iterator_category,bidirectional_iterator_tag>::value |
-		is_same<typename U::iterator_category,input_iterator_tag>::value |
-		is_same<typename U::iterator_category,forward_iterator_tag>::value |
-	is_same<typename U::iterator_category,random_access_iterator_tag>::value;
-};
-
 #endif
